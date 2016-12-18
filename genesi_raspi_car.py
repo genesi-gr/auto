4e4b86e881dd0f021f3d296eafe869dcf6448045
Ευάγγελος <evan@emeis.tech>
2016-12-18 10:37:21 -0500

    Merged files into a single file

--- genesi_raspi_car.py
+++ genesi_raspi_car.py
@@ -1,8 +1,25 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import websocket
+import hmac
+import urllib
+import urllib.request
+
 try:
     import RPi.GPIO as GPIO
     on_ras = True
 except ImportError:
     on_ras = False 
+
+def websocket_send(url, message, hmac_key=None, group='default'):
+    sig = hmac_key.encode() and hmac.new(hmac_key.encode(), message.encode()).hexdigest() or ''
+    params = urllib.parse.urlencode(
+        {'message': message, 'signature': sig, 'group': group})
+    f = urllib.request.urlopen(url, params.encode())
+    data = f.read()
+    f.close()
+    return data
 
 class auto( object ):
 
@@ -18,14 +35,14 @@ class auto( object ):
             GPIO.setmode( GPIO.BCM )
             p_pwm = []
             for pin in self.pwm_pin:
-                print 'Setting up pin %d'%pin
+                print('Setting up pin %d'%pin)
                 GPIO.setup( pin , GPIO.OUT )
                 GPIO.PWM( pin , freq )
                 p_pwm.append( GPIO.PWM( pin , freq ) )
                 p_pwm[-1].start( 100 )
             self.p_pwm = p_pwm
             for pin in self.binary_pin:
-                print 'Setting up pin %d'%pin
+                print('Setting up pin %d'%pin)
                 GPIO.setup( pin , GPIO.OUT )
                 GPIO.output( pin , True )
                 
@@ -68,3 +85,46 @@ class auto( object ):
                 p.ChangeDutyCycle( 100. )
             for pin in self.binary_pin:
                 GPIO.output( pin , True )
+
+diakomistns = 'genesi.gr:1234/'
+diaulos     = 'genesi.gr:1234/realtime/'
+
+car = auto()
+
+def on_msg( ws , msg ):
+
+    print('Μήνυμα:', msg)
+    if 'ηχώ' in msg:
+        print('ηχώ')
+        websocket_send('http://' + diakomistns , msg , pwd , 'ηχώ')
+    elif 'νεκρά' in msg:
+        print('Νεκρά')
+        car.vekra()
+    elif 'διακοπή' in msg:
+        os.system( 'poweroff' )
+    else:
+        if 'πρόσω' in msg:
+            taxutnta = int( msg[-3:] )
+            print('Πρόσω')
+            car.prosw( taxutnta )
+        elif 'ανάποδα' in msg:
+            taxutnta = int( msg[-3:] )
+            print('Ανάποδα')
+            car.anapoda( taxutnta )
+        if 'αριστερά' in msg:
+            print('Αριστερά')
+            car.aristera()
+        elif 'δεξιά' in msg:
+            print('Δεξιά')
+            car.deksia()
+
+def onError( self , error_msg ):
+    print(error_msg)
+
+if __name__ == '__main__':
+
+    omada = sys.argv[1]
+    ws = websocket.WebSocketApp('ws://' + diaulos + omada , on_message=on_msg)
+    print('Συνδέθηκε η ομάδα', omada)
+    ws.run_forever()
+
