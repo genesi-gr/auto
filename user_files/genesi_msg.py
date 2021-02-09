b07f32ada403556474994f3d1ded5cf73d5579e9
evan54 <evan54@gmail.com>
2021-02-09 02:46:20 -0500

    minor style updates

--- user_files/genesi_msg.py
+++ user_files/genesi_msg.py
@@ -3,31 +3,40 @@ from websocket_messaging import websocket_send
 
 class car(object):
 
-    def __init__( self , carName ):
-        self.carName = carName
-        self.socket_server = 'http://genesi.gr:1234'
-        self.pwd = input('Κωδικός ')
+    def __init__(self, car_name):
+        """Διαθέσιμες εντολές:
+        prosw()
+        anapoda()
+        aristera()
+        deksia()
+        vekra()
+        """
+        self.car_name = car_name
+        self._socket_server = 'http://genesi.gr:1234'
+        self._pwd = input('Κωδικός ')
 
-    def carSend( self , msg ):
-        websocket_send( self.socket_server , msg , self.pwd , self.carName )
+    def _car_send(self, msg):
+        websocket_send(self._socket_server, msg, self._pwd, self.car_name)
 
-    def nxw( self ):
-        self.carSend( 'ηχώ' )
+    def nxw(self):
+        self._car_send('ηχώ')
 
-    def prosw( self , taxutnta=50. ):
-        self.carSend( 'πρόσω%3d'%taxutnta )
+    def prosw(self, taxutnta=50.):
+        self._car_send('πρόσω%3d'%taxutnta)
 
-    def anapoda( self , taxutnta=50. ):
-        self.carSend( 'ανάποδα%3d'%taxutnta )
+    def anapoda(self, taxutnta=50.):
+        self._car_send('ανάποδα%3d'%taxutnta)
 
-    def aristera( self ):
-        self.carSend( 'αριστερά' )
+    def aristera(self):
+        self._car_send('αριστερά')
 
-    def deksia( self ):
-        self.carSend( 'δεξιά' )
+    def deksia(self):
+        self._car_send('δεξιά')
 
-    def vekra( self ):
-        self.carSend( 'νεκρά' )
+    def vekra(self):
+        self._car_send('νεκρά')
 
-    def diakopn( self ):
-        self.carSend( 'διακοπή' )
+    def diakopn(self):
+        answer = input('Είσαι σίγουρος ότι θες να σβήσεις το αυτοκινητάκι;[ναι/όχι]').lower()
+        if answer in ['nai', 'vai', 'yes', 'y']:
+            self._car_send('διακοπή')
